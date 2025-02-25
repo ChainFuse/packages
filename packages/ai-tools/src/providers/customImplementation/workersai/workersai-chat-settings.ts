e73b4009900bebf084a24f9aa3598ddc11d280a6
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-02-25 11:55:45 -0700

    Drop custom code

--- packages/ai-tools/src/providers/customImplementation/workersai/workersai-chat-settings.ts
+++
@@ -1,5 +0,0 @@
-import type { BaseAiSummarization, BaseAiTextEmbeddings, BaseAiTextGeneration } from '@cloudflare/workers-types/experimental';
-
-export type WorkersAiChatSettings = Omit<BaseAiTextGeneration['inputs'], 'prompt' | 'messages' | 'tools'>;
-export type WorkersAiSummarizationSettings = Omit<BaseAiSummarization['inputs'], 'input_text'>;
-export type WorkersAiEmbeddingSettings = Omit<BaseAiTextEmbeddings['inputs'], 'text'>;
