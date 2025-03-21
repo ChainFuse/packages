12b04f9618ece154fd4b2ea80cf0bacd36bd759f
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-03-21 11:43:32 -0700

    Export azure ai tools

--- packages/types/src/ai-tools/azure/index.ts
+++ packages/types/src/ai-tools/azure/index.ts
@@ -1,3 +1,5 @@
-type AzureChatModels = 'gpt-35-turbo' | 'gpt-4-turbo' | 'gpt-4o' | 'gpt-4o-mini' | 'o1' | 'o3-mini';
-type AzureImageModels = 'dall-e-3';
-type AzureEmbeddingModels = 'text-embedding-3-small' | 'text-embedding-3-large';
+import type { azureCatalog } from './catalog.js';
+
+export type AzureChatModels = (typeof azureCatalog)[number]['languageModelAvailability'][number]['name'];
+// type AzureImageModels = (typeof azureCatalog)[number]['imageModelAvailability'][number];
+export type AzureEmbeddingModels = (typeof azureCatalog)[number]['textEmbeddingModelAvailability'][number];
