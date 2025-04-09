1b5fc40fe519aa388f077b90f2b94f4990f85c2e
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-04-09 14:41:23 -0700

    copy pasta

--- packages/types/src/wf/index.ts
+++ packages/types/src/wf/index.ts
@@ -15,7 +15,7 @@ export const ZodUuidExportInput = z.union([
 		.string()
 		.trim()
 		.length(32)
-		.refine((values) => import('validator/es/lib/isHexadecimal').then(({ default: isHexadecimal }) => isHexadecimal(values)).catch(() => import('validator').then(({ default: validator }) => validator.isHexadecimal(values)))),
+		.refine((value) => import('validator/es/lib/isHexadecimal').then(({ default: isHexadecimal }) => isHexadecimal(value)).catch(() => import('validator').then(({ default: validator }) => validator.isHexadecimal(value)))),
 	z.string().trim().nonempty().base64(),
 	z.string().trim().nonempty().base64url(),
 ]);
