23201123d3de2c137486c39dafd7cacd9551071e
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-05-20 13:42:00 -0700

    Force import from v3

---
+++ .changeset/upset-dots-relate.md
@@ -0,0 +1,5 @@
+---
+'@chainfuse/types': patch
+---
+
+types(fix): Force import from v3
--- packages/types/src/zod/index.ts
+++ packages/types/src/zod/index.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from 'zod/v3';
 
 /**
  * @link https://zod.dev/?id=json-type
