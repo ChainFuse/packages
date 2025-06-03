c0ccafec0a1eb0e72bbc67edbf313af870d37c22
George Portillo <georgeportillodesign@gmail.com>
2025-06-03 10:18:45 -0700

    Add top level imports instead

--- packages/types/src/d0/index.ts
+++ packages/types/src/d0/index.ts
@@ -1,26 +1,25 @@
+import { z as z3 } from 'zod/v3';
+import { z as z4 } from 'zod/v4';
+
 export type D0Blob = [number, ...number[]];
 
 /**
  * @deprecated
  */
-const PrefixedUuidRaw3 = await import('zod/v3').then(({ z }) =>
-	z
-		.string()
-		.trim()
-		.toLowerCase()
-		.min(38)
-		.max(40)
-		.regex(new RegExp(/^((d|t|u)_)?[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}(_p)?$/i)),
-);
-export const PrefixedUuidRaw = await import('zod/v4').then(({ z }) =>
-	z
-		.string()
-		.trim()
-		.toLowerCase()
-		.min(38)
-		.max(40)
-		.regex(new RegExp(/^((d|t|u)_)?[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}(_p)?$/i)),
-);
+const PrefixedUuidRaw3 = z3
+	.string()
+	.trim()
+	.toLowerCase()
+	.min(38)
+	.max(40)
+	.regex(new RegExp(/^((d|t|u)_)?[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}(_p)?$/i));
+export const PrefixedUuidRaw = z4
+	.string()
+	.trim()
+	.toLowerCase()
+	.min(38)
+	.max(40)
+	.regex(new RegExp(/^((d|t|u)_)?[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}(_p)?$/i));
 
 /**
  * @deprecated
@@ -57,40 +56,36 @@ export const PrefixedUuid = PrefixedUuidRaw.transform((value) => {
 /**
  * @deprecated
  */
-export const ZodUuid3 = await import('zod/v3').then(({ z }) =>
-	z.union([
-		PrefixedUuidRaw3,
-		// utf-8
-		z
-			.string()
-			.trim()
-			.toLowerCase()
-			.uuid()
-			.refine((value) => import('validator/es/lib/isUUID').then(({ default: isUUID }) => isUUID(value, 7)).catch(() => import('validator').then(({ default: validator }) => validator.isUUID(value, 7)))),
-		// hex
-		z
-			.string()
-			.trim()
-			.toLowerCase()
-			.length(32)
-			.refine((value) => import('validator/es/lib/isHexadecimal').then(({ default: isHexadecimal }) => isHexadecimal(value)).catch(() => import('validator').then(({ default: validator }) => validator.isHexadecimal(value)))),
-		z.string().trim().nonempty().base64(),
-		z.string().trim().nonempty().base64url(),
-	]),
-);
-export const ZodUuid = await import('zod/v4').then(({ z }) =>
-	z.union([
-		PrefixedUuidRaw,
-		// utf-8
-		z.uuidv7().trim().toLowerCase(),
-		// hex
-		z
-			.string()
-			.trim()
-			.toLowerCase()
-			.length(32)
-			.refine((value) => import('validator/es/lib/isHexadecimal').then(({ default: isHexadecimal }) => isHexadecimal(value)).catch(() => import('validator').then(({ default: validator }) => validator.isHexadecimal(value)))),
-		z.base64().trim().nonempty(),
-		z.base64url().trim().nonempty(),
-	]),
-);
+export const ZodUuid3 = z3.union([
+	PrefixedUuidRaw3,
+	// utf-8
+	z3
+		.string()
+		.trim()
+		.toLowerCase()
+		.uuid()
+		.refine((value) => import('validator/es/lib/isUUID').then(({ default: isUUID }) => isUUID(value, 7)).catch(() => import('validator').then(({ default: validator }) => validator.isUUID(value, 7)))),
+	// hex
+	z3
+		.string()
+		.trim()
+		.toLowerCase()
+		.length(32)
+		.refine((value) => import('validator/es/lib/isHexadecimal').then(({ default: isHexadecimal }) => isHexadecimal(value)).catch(() => import('validator').then(({ default: validator }) => validator.isHexadecimal(value)))),
+	z3.string().trim().nonempty().base64(),
+	z3.string().trim().nonempty().base64url(),
+]);
+export const ZodUuid = z4.union([
+	PrefixedUuidRaw,
+	// utf-8
+	z4.uuidv7().trim().toLowerCase(),
+	// hex
+	z4
+		.string()
+		.trim()
+		.toLowerCase()
+		.length(32)
+		.refine((value) => import('validator/es/lib/isHexadecimal').then(({ default: isHexadecimal }) => isHexadecimal(value)).catch(() => import('validator').then(({ default: validator }) => validator.isHexadecimal(value)))),
+	z4.base64().trim().nonempty(),
+	z4.base64url().trim().nonempty(),
+]);
