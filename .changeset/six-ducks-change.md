1d8db06e5e6f05af79b6c9444144514feca330ae
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-08-05 13:58:44 -0700

    Export JSON type for external use

---
+++ .changeset/six-ducks-change.md
@@ -0,0 +1,5 @@
+---
+'@chainfuse/types': patch
+---
+
+types(feat): Export JSON type for external use
--- packages/types/src/zod/index.ts
+++ packages/types/src/zod/index.ts
@@ -4,8 +4,8 @@ import { z } from 'zod/v3';
  * @link https://zod.dev/?id=json-type
  */
 const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
-type Json = z.infer<typeof literalSchema> | { [key: string]: Json } | Json[];
-export const jsonSchema: z.ZodType<Json> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]));
+export type JSON = z.infer<typeof literalSchema> | { [key: string]: JSON } | JSON[];
+export const jsonSchema: z.ZodType<JSON> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]));
 
 export const ZodCoordinate = await import('zod/v4').then(({ z }) =>
 	z
