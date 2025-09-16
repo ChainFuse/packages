17e5cfaac91b79163cecfa7b029bed80c744919c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-09-16 16:45:00 -0700

    Add new enums

--- packages/types/src/d0/index.ts
+++ packages/types/src/d0/index.ts
@@ -1,6 +1,37 @@
 export type D0Blob = [number, ...number[]];
 
-export enum ShardType {
+export enum D0Version {
+	v0 = 0,
+}
+
+export enum D0SystemType {
+	Dataspace = 0,
+	Tenant = 1,
+	User = 2,
+	Workflow = 3,
+}
+
+export enum D0CombinedLocations {
+	none = 0,
+	eu = 1,
+	fedramp = 2,
+	wnam = 10,
+	enam = 11,
+	sam = 12,
+	weur = 13,
+	eeur = 14,
+	apac = 15,
+	oc = 16,
+	afr = 17,
+	me = 18,
+}
+
+export enum D0ShardType {
 	Director = 0,
 	Storage = 1,
+}
+
+export enum D0Environment {
+	Production = 0,
+	Preview = 1,
 }
